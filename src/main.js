ba23745c31ae56d1feb2fefecb1e63b97aab3200
Rosalini Samuel <rosalinisamuel@gmail.com>
2022-07-25 19:31:48 -0300

    Primera migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,65 +1,95 @@
 
-// const api = require('axios').default({
-//     baseURL: 'https://api.themoviedb.org/3/',
-//     headers:{
-//         'Content-Type': 'application/json;charset=utf-8',
-//     },
-//     params: {
-//         'api_key': API_KEY,
-//     },
-//  })
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY
+    },
+});
 
 
- async function getTrendingMoviesPreviewe (){
-     const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
 
-     const data = await res.json();
-     const movies = data.results;
-     console.log(movies);
+//funcion peliculas en tendencia
 
-     movies.forEach(movie => {
-         const movieTrendingSection = document.querySelector('#trendingPreview .trendingPreview-movieList');
-         const movieContainer = document.createElement('div');
-         movieContainer.classList.add('movie-container');
+//  async function getTrendingMoviesPreviewe (){
+//      const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
 
-         const movieImg = document.createElement('img');
-         movieImg.classList.add('movie-img');
-         movieImg.setAttribute('alt', movie.title);
-         movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300/'+ movie.poster_path);
+//      const data = await res.json();
+//      const movies = data.results;
+//      console.log(movies);
 
-         movieContainer.appendChild(movieImg);
-         movieTrendingSection.appendChild(movieContainer);
-     });
- }
+//      movies.forEach(movie => {
+//          const movieTrendingSection = document.querySelector('#trendingPreview .trendingPreview-movieList');
+//          const movieContainer = document.createElement('div');
+//          movieContainer.classList.add('movie-container');
 
-// //migracion axios
-// async function getTrendingMoviesPreviewe (){
-//     const { data } = await api ('trending/movie/day');
-//     const movies = data.results;
-//     console.log(movies);
+//          const movieImg = document.createElement('img');
+//          movieImg.classList.add('movie-img');
+//          movieImg.setAttribute('alt', movie.title);
+//          movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300/'+ movie.poster_path);
 
-//     movies.forEach(movie => {
-//         const movieTrendingSection = document.querySelector('#trendingPreview .trendingPreview-movieList');
-//         const movieContainer = document.createElement('div');
-//         movieContainer.classList.add('movie-container');
+//          movieContainer.appendChild(movieImg);
+//          movieTrendingSection.appendChild(movieContainer);
+//      });
+//  }
 
-//         const movieImg = document.createElement('img');
-//         movieImg.classList.add('movie-img');
-//         movieImg.setAttribute('alt', movie.title);
-//         movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300/'+ movie.poster_path);
 
-//         movieContainer.appendChild(movieImg);
-//         movieTrendingSection.appendChild(movieContainer);
-//     });
-// }
+//migracion a axios
+async function getTrendingMoviesPreviewe (){
+    const { data } = await api ('trending/movie/day');
+
+    const movies = data.results;
+    console.log(movies);
+
+    movies.forEach(movie => {
+        const movieTrendingSection = document.querySelector('#trendingPreview .trendingPreview-movieList');
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300/'+ movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        movieTrendingSection.appendChild(movieContainer);
+    });
+}
+
 
 
 //funcion listar categorias peliculas
 
-async function getCategoriesMoviesPreview(){
-    const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+// async function getCategoriesMoviesPreview(){
+//     const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
 
-    const data = await res.json();
+//     const data = await res.json();
+//     const categories = data.genres;
+    
+//     categories.forEach(category => {
+//         const categorySection = document.querySelector('#categoriesPreview .categoriesPreview-list');
+//         const categoryContainer = document.createElement('div');
+//         categoryContainer.classList.add('category-container');
+
+//         const categoryTitle = document.createElement('h3');
+//         categoryTitle.classList.add('category-title');
+//         categoryTitle.setAttribute('id', 'id' + category.id);
+//         const categoryTitleText = document.createTextNode(category.name);
+
+//         categoryTitle.appendChild(categoryTitleText);
+//         categoryContainer.appendChild(categoryTitle);
+//         categorySection.appendChild(categoryContainer);
+//     });
+// }
+
+
+//migracion axios
+async function getCategoriesMoviesPreview(){
+    const { data } = await api ('genre/movie/list');
+
     const categories = data.genres;
     
     categories.forEach(category => {
